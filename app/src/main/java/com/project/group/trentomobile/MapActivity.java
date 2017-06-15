a10ef00f7ed156bd69370323cf7790ad8128064c
Unknown <sptanco@gmail.com>
2017-06-15 14:26:03 +0200

    Map categorie - click marker -click info windows - go to tail activity

--- app/src/main/java/com/project/group/trentomobile/MapActivity.java
+++ app/src/main/java/com/project/group/trentomobile/MapActivity.java
@@ -1,5 +1,6 @@
 package com.project.group.trentomobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,13 +17,15 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.project.group.trentomobile.Classi.Luogo;
 import com.project.group.trentomobile.Classi.Tile;
+import com.project.group.trentomobile.Repository.TileMemoryRep;
 
 import java.util.ArrayList;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap;
     private ArrayList<Tile> tiles;
@@ -46,12 +50,27 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback{
         for(Tile t : tiles) {
             if(t instanceof Luogo) {
                 LatLng Trento = new LatLng(((Luogo) t).getIndirizzo().getLat(), ((Luogo) t).getIndirizzo().getLng());
-                mMap.addMarker(new MarkerOptions().position(Trento).title(t.getTitolo()));
+                Marker m = mMap.addMarker(new MarkerOptions().position(Trento).title(t.getTitolo()));
+                m.setTag(t.getId());
             }
         }
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                 new LatLng(46.067197, 11.121376), 14));
 
+        mMap.setOnInfoWindowClickListener(this);
+
+    }
+
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Tile t = TileMemoryRep.getInstance().getTileById((Integer) marker.getTag());
+
+        Intent myIntent = new Intent(this, TailActivity.class);
+
+        //the name of optional parameters must include a package prefix (Ex. 'com.project.group.trentomobile.TilePK.TileData')
+        myIntent.putExtra("data", t); //Optional parameters
+        this.startActivity(myIntent);
     }
 }
