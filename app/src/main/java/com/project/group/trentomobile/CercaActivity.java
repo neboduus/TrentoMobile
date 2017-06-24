aa855afef57abdc9f0f1015cab56b37e6dd8cc41
Unknown <sptanco@gmail.com>
2017-06-24 09:14:28 +0200

    cerca fermate

--- app/src/main/java/com/project/group/trentomobile/CercaActivity.java
+++ app/src/main/java/com/project/group/trentomobile/CercaActivity.java
@@ -11,14 +11,24 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.project.group.trentomobile.Classi.Fermata;
+import com.project.group.trentomobile.Classi.Indirizzo;
 import com.project.group.trentomobile.Classi.Preferenze;
 import com.project.group.trentomobile.Classi.Tile;
 import com.project.group.trentomobile.Repository.TileMemoryRep;
 import com.project.group.trentomobile.TilePK.TileFragment;
 import com.project.group.trentomobile.Util.InternalStorage;
+import com.project.group.trentomobile.assetsHelper.SQLAssetHelper_DB;
+import com.project.group.trentomobile.transport.Linea;
+import com.project.group.trentomobile.transport.Orario;
+import com.project.group.trentomobile.transport.Stop;
+import com.project.group.trentomobile.transport.Trip;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 public class CercaActivity extends AppCompatActivity {
@@ -60,8 +70,36 @@ public class CercaActivity extends AppCompatActivity {
             //Cerca nel titolo e in base alla categoria
             if(tile.getTitolo().contains(cerca))
             {
-                tiles.add(tile);
+                if(!(tile instanceof Fermata))
+                    tiles.add(tile);
             }
+        }
+
+        SQLAssetHelper_DB sqlDB = new SQLAssetHelper_DB(this);
+
+        List<Stop> stops = sqlDB.getStopsByName(cerca);
+
+
+        for(Stop s : stops){
+
+            List<Orario> lo = sqlDB.getNearestOrarioFromStop(s,new SimpleDateFormat("HH:mm:ss").format(new Date()));
+            String corpo="";
+            Indirizzo indirizzo = new Indirizzo(s.getLat(), s.getLon(), null);
+
+            int count = 1;
+
+            for(Orario o : lo){
+                o.getArrival_time();
+                Trip t = sqlDB.getTripById(o.getTrip_id());
+                Linea l = sqlDB.getLineaById(t.getRoute_id());
+
+                corpo+="Bus:"+l.getShort_name()+"\n\tpartenza:"+o.getArrival_time()+"\n\t"+"direzione:"+(t.getDirection_id() ? "andata" :"ritorno")+"\n";
+
+                if(count==0) break;
+                count--;
+            }
+
+            tiles.add(new Fermata(s.getId(),s.getName(),corpo,"https://png.icons8.com/bus/color/50","https://png.icons8.com/bus/color/50",indirizzo));
         }
 
 
